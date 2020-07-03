25333eb328ff83b4c7b7c758097664f34ea27c4e
Zen-net <allyofjustice1@gmail.com>
2020-07-03 23:56:45 +0700

    Updates

---
+++ precache-manifest.12628707f1e4667cfd8933c0fd4e294e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b387d636fce96ca9874b10d638e8bb00",
+    "url": "/tic-tac-toe-react/index.html"
+  },
+  {
+    "revision": "6dcfa15e7dc2c49f0555",
+    "url": "/tic-tac-toe-react/static/js/2.fea4e0ec.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/tic-tac-toe-react/static/js/2.fea4e0ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38f806a12241bf2d9efc",
+    "url": "/tic-tac-toe-react/static/js/main.dc75d6f7.chunk.js"
+  },
+  {
+    "revision": "7cff477e1e5f875c48a8",
+    "url": "/tic-tac-toe-react/static/js/runtime-main.e4d44163.js"
+  }
+]);
